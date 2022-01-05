5db7339d091cdc07573c982e87f2b978ed7aba64
PiyushSBorse <72652554+PiyushSBorse@users.noreply.github.com>
2022-01-05 18:56:00 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+# On Branch branch1
+
+# Add the Implemention
+def add(x, y):
+    return x+y
+	
+# Subtraction Implemention
+def subtract(x, y):
+    return x-y
+
+def multiply(x, y):
+	pass
+	
+# Division Implementation
+def divide(x, y):
+	pass
